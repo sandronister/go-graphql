0a526ec772ab3ae27b5839b8109fb1b269aa41d7
Julio Sandroni <sandronister@gmail.com>
2023-11-25 14:32:58 -0300

    chore:chain category

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,20 +2,6 @@
 
 package model
 
-type Category struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
-	Courses     []*Course `json:"courses"`
-}
-
-type Course struct {
-	ID          string    `json:"id"`
-	Name        *string   `json:"name,omitempty"`
-	Description *string   `json:"description,omitempty"`
-	Category    *Category `json:"category"`
-}
-
 type NewCategory struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description,omitempty"`
--- internal/database/course.go
+++ internal/database/course.go
@@ -55,3 +55,24 @@ func (c *Course) FindAll() ([]Course, error) {
 
 	return courses, nil
 }
+
+func (c *Course) FindByCategoryId(categoryID string) ([]Course, error) {
+	rows, err := c.db.Query("SELECT id,name, description, category_id FROM courses WHERE category_id = $1", categoryID)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	courses := []Course{}
+
+	for rows.Next() {
+		var id, name, description, categoryID string
+		if err := rows.Scan(&id, &name, &description, &categoryID); err != nil {
+			return nil, err
+		}
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: categoryID})
+	}
+
+	return courses, nil
+}
